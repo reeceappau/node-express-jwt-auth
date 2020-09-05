1cfa1e289a85eef0e5cc88281ddd23f4acd17457
Reece Appau <reeceappau.ma@gmail.com>
2020-09-05 22:47:10 +0000

    Added conditionall rendering

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -11,6 +12,7 @@ const requireAuth = (req, res, next) => {
                 console.log(err.message);
                 res.redirect("/login");
             } else {
+                console.log(decodedToken);
                 next();
             }
         });
@@ -19,4 +21,26 @@ const requireAuth = (req, res, next) => {
     }
 };
 
-module.exports = { requireAuth };
+//check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "frjndgj", async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+};
+
+module.exports = { requireAuth, checkUser };
